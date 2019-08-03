065927be5b8ef9e546deb42b60d737adc9022d2a
zuoquanGong <2754915212@qq.com>
2019-08-03 17:58:51 +0800

    add function: get embedding

--- SimpleFeeder/SimpleFeeder/SimpleFeeder.py
+++ SimpleFeeder/SimpleFeeder/SimpleFeeder.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 """
 
-@Project: ESIM
+@Project: SimpleFeeder
 @File   : SimpleFeeder.py
 @Time   : "2019/7/30 21:00
 @Author  : ZuoquanGong
@@ -17,21 +17,26 @@
 # 标注说明：label:0 ''     sent:0             sent:1       sfeat:0 ''
 # '' 部分将被省略掉，sfeat 代表 sparse feature，dfeat 代表 dense feature
 
-from typing import List
+from typing import Dict
 import torch.nn as nn
 import numpy as np
 from torch import LongTensor, ByteTensor, FloatTensor
 from torch.autograd import Variable
 import re
 
-from ComponentCategory import LabelGroup, SentenceGroup, DenseFeatureGroup, SparseFeatureGroup
+# from ComponentCategory import LabelGroup, SentenceGroup, DenseFeatureGroup, SparseFeatureGroup
+# # from Readers import SICK_reader, POS_reader, conllu_reader
+# from Readers import *
+# from Vocabulary import Vocabulary
+
+from SimpleFeeder.ComponentCategory import LabelGroup, SentenceGroup, DenseFeatureGroup, SparseFeatureGroup
 # from Readers import SICK_reader, POS_reader, conllu_reader
-from Readers import *
-from Vocabulary import Vocabulary
+from SimpleFeeder.Readers import *
+from SimpleFeeder.Vocabulary import Vocabulary
 
 
 # =============================================================================
-# 一、InstanceList
+#  InstanceList
 # =============================================================================
 # 所有Instance的容器，相当于一个数据集
 # 可以在其中完成常规数据处理流程
@@ -40,7 +45,6 @@ class InstanceList(List):
     has_sent_pretrain = False
     embed_dim = 0
     batch_size = 0
-    batch_num = 0
     is_POS = False
 
     def __init__(self,
@@ -48,6 +52,7 @@ class InstanceList(List):
         super(InstanceList, self).__init__()
         self.is_train = is_train
         self.indices = []
+        self.batch_num = 0
 
     # 1.从文件中加载数据
     def read(self,
@@ -74,6 +79,9 @@ class InstanceList(List):
         else:
             self.extend(reader(path, read_model, separator=separator, minicut=minicut))
         self.indices = list(range(len(self)))
+        if self.is_train:
+            self.set_train()
+        print("Loading is finished -- {} insts ".format(len(self)))
 
     # 2.建立词表
     def build_vocab(self):
@@ -87,6 +95,7 @@ class InstanceList(List):
     # 3.将文本转化为索引
     def txt2idx(self):
         if not self.is_train:
+            print("warning: can't execute this function for this dataset, please set it trainning（set_train）.")
             return
         LabelGroup.txt2idx()
         SentenceGroup.txt2idx()
@@ -149,13 +158,13 @@ class InstanceList(List):
         print('Extra_embedding is done.')
 
     # 5.建立词嵌入
-    def build_pretrain(self,
+    def build_sent_pretrain(self,
                        embed_dim: int,
                        key='sent'):
         if not self.has_sent_pretrain:
-            self.embed_dim=embed_dim
+            self.embed_dim = embed_dim
         embed = nn.Embedding(len(SentenceGroup.vocab_group[key]), self.embed_dim)
-        embed.weight.requires_grad=True
+        embed.weight.requires_grad = True
         SentenceGroup.embed_group[key] = embed
 
     # 6.batch生成器
@@ -167,12 +176,15 @@ class InstanceList(List):
         self.batch_num = len(self) // batch_size
 
         class BatchData:
+            size = 0
+
             def __init__(self,
                          batch_tensor_dict,
                          batch_tensor_dict_ext):
                 self.batch_tensor_dict = batch_tensor_dict
                 self.batch_tensor_dict_ext = batch_tensor_dict_ext
 
+        BatchData.size = self.batch_size
         for batch in range(self.batch_num):
             batch_indices = self.indices[batch * batch_size:(batch + 1) * batch_size]
 
@@ -200,6 +212,7 @@ class InstanceList(List):
     ###############
     # 7.设置当前数据集为训练集
     def set_train(self):
+        self.is_train = True
         for inst in self:
             for attr in inst.attr.values():
                 attr.is_train = True
@@ -269,89 +282,16 @@ class InstanceList(List):
             tensor.to(device=device)
             return tensor
 
+    def get_pretrain(self) -> Dict:
+        embed = {}
+        embed.update(SentenceGroup.embed_group)
+        # embed.update(DenseFeatureGroup.embed_group)
+        # embed.update(SparseFeatureGroup.embed_group)
+        return embed
 
-# =============================================================================
-# 二、Tests and Examples
-# =============================================================================
-# 使用流程：
-# 首先，定义数据路径（path），数据读取格式（read_model），读取方法（reader），组分间分隔符和成分内分隔符（separator，minicut)
-# 注：例如 "A-component \t B-component \t C-component"，A、B、C组分间的分隔符为“\t”
-# 注：C-component 假设是句子 “W1<space>W2<space>W3<space>W4” ，则 minicut 为“<space>”
-# 然后，进入流程，依次调用 InstanceList的 read、set_train、build_vocab、build_pretrain、txt2idx、batch_generator
-# batch_generator的输出即可以满足模型输入
-
-if __name__ == "__main__":
-    train = InstanceList(is_train=True)
-
-    '''
-    #pair_ID	sentence_A	sentence_B	relatedness_score	entailment_judgment
-    4	The young boys are playing outdoors and the man is smiling nearby	There is no boy playing outdoors and there is no man smiling	3.6	CONTRADICTION
-    24	A person in a black jacket is doing tricks on a motorbike	A skilled person is riding a bicycle on one wheel	3.4	NEUTRAL
-    '''
-    # path = 'G:\桌面\语料搜集\SICK.txt'
-    # read_model = ['', 'sent:A', 'sent:B', '', 'label:entail']
-    # reader = SICK_reader
-    # train.read(path, read_model, reader=reader, separator='\t', minicut=' ')
-
-    '''
-    #has_relation   sentence_A  sentence_B  sparse_feature_dist
-    1	房间 很 大 ， 用品 设施 齐全 ， 还有 麻将 室 和 自动 麻将桌 ， 虽然 没 用上 吧 ； 工作 人员 服务 态度 不错 ； 因为 挨着 高架桥 ， 晚上 会 有 车辆 穿行 的 声音 ， 不过 总体 来说 还 不错 ， 性价比 蛮 高 的 。	性价比 蛮 高的	12
-    0	钟楼 附近 ， 观景 购物 很 方便 ， 去 哪儿 网 订 的 房间 ， 并 不 贵 ， 398 元 ， 房间 空间 大 ， 有 近 40 平米 ， 淋浴 不错 。	房间 观景 购物 很 方便	77
-    '''
-    # path = 'G:\桌面\语料搜集\opinion_relation.txt'
-    # read_model = ['label:rel', 'sent:A', 'sent:B', 'sfeat_num:dist']
-    # reader = SICK_reader
-    # train.read(path, read_model, reader=reader, separator='\t', minicut=' ')
-
-    '''
-    #has_relation   sentence_A  sentence_B  sparse_feature_dist sparse_feature_polar
-    1	环境 很 好 ， 在 夫子庙 景区 中心 ， 但 房间 设施 陈旧 ， 特别是 卫生间 座便 目不忍睹 ， 应该 换了 。	环境 很好	11 1
-    0	环境 很 好 ， 在 夫子庙 景区 中心 ， 但 房间 设施 陈旧 ， 特别是 卫生间 座便 目不忍睹 ， 应该 换了 。	环境 陈旧	87 0
-    '''
-    # path = 'G:\桌面\语料搜集\opinion_relation2.txt'
-    # read_model = '[label:rel]	[sent:A]	[sent:B]	[sfeat_num:dist] [sfeat:polar]'
-    # reader = SICK_reader
-    # train.read(path, read_model, reader=reader, separator='\t', minicut=' ')
-
-    '''
-    上海_NR 浦东_NR 开发_NN 与_CC 法制_NN 建设_NN 同步_VV 
-    新华社_NN 上海_NR 二月_NT 十日_NT 电_NN （_PU 记者_NN 谢金虎_NR 、_PU 张持坚_NR ）_PU 
-    '''
-    # path = 'G:\桌面\语料搜集\pos.txt'
-    # read_model = ['sent:word', 'label:tag']
-    # reader = POS_reader
-    # train.read(path, read_model, reader=reader, separator=' ', minicut='_')
-
-    '''
-        # sent_id = train-s111
-        # text = 喇叭花是温带植物。
-        1	喇叭花	喇叭花	NOUN	NN	_	4	nsubj	_	SpaceAfter=No
-        2	是	是	AUX	VC	_	4	cop	_	SpaceAfter=No
-        3	温带	温带	NOUN	NN	_	4	nmod	_	SpaceAfter=No
-        4	植物	植物	NOUN	NN	_	18	dep	_	SpaceAfter=No
-        5	。	。	PUNCT	.	_	18	punct	_	SpaceAfter=No
-        '''
-    # path = 'G:\桌面\语料搜集\conllu.txt'
-    # read_model = ['', 'sent:A', '', 'dfeat:pos', '', '', '', 'label:dep', '', '']
-    # reader = conllu_reader
-    # train.read(path, read_model, reader, no_fix=True)
-
-    print(train[0])
-    print(train[1])
-    print(train[2])
-    train.set_train()  # 给予数据集操作权限
-    train.build_vocab()  # 建立词表
-    print(LabelGroup.vocab_group)
-    print(DenseFeatureGroup.vocab_group)
-    # print(SparseFeatureGroup.vocab_group)
-    # print(SparseFeatureGroup.vocab_group['sfeat_num'])
-    # print(SparseFeatureGroup.vocab_group['sfeat'])
-    train.build_pretrain(2)  # 建立词嵌入——Embedding
-    train.txt2idx()  # 将text数据映射成index数据
-    print(train[0])
-    print(train[1])
-    print(train[2])
-    for i, batch in enumerate(train.batch_generator(32)):  # batch 生成器
-        if i > 0:
-            break
-        print(batch.__dict__.items())
+    def get_pretrain_ext(self):
+        embed = {}
+        embed.update(SentenceGroup.ext_embed_group)
+        # embed.update(DenseFeatureGroup.embed_group)
+        # embed.update(SparseFeatureGroup.embed_group)
+        return embed
